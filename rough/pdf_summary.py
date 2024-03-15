e61def2b0346b7804dca69cea84cad05641364d3
Ashlo <ashutoshbele5@gmail.com>
2024-03-15 13:43:42 +0530

    added gradio support

--- rough/pdf_summary.py
+++ rough/pdf_summary.py
@@ -1,4 +1,5 @@
 #load document
+import gradio as gr
 from langchain_community.llms import Ollama
 from langchain.document_loaders import PyPDFLoader
 from langchain.chains.summarize import load_summarize_chain
@@ -12,7 +13,38 @@ def summarize_pdf(pdf_file_path, custom_prompt=""):
     docs = loader.load_and_split()
     chain = load_summarize_chain(llm, chain_type="map_reduce")
     summary = chain.invoke(docs)
-    print(summary)
+    summary = format_summary_to_bullet_points(summary)
     return summary
 
-summarize_pdf('Demo_Patient_Report.pdf', custom_prompt="summarize pdf")
+def format_summary_to_bullet_points(summary_data):
+    # Extract the 'output_text' field from the summary data
+    output_text = summary_data.get('output_text', '')
+
+    # Split the output text into sentences
+    sentences = output_text.split('. ')
+
+    # Format each sentence as a bullet point
+    bullet_points = ["- " + sentence.strip() for sentence in sentences if sentence]
+
+    # Combine the bullet points into a single string
+    formatted_summary = "\n".join(bullet_points)
+
+    return formatted_summary
+
+def main():
+    input_pdf_path = gr.File(label="Upload PDF")
+    output_summary=  gr.Textbox(label="Summary")
+
+    iface = gr.Interface(
+        fn=summarize_pdf,
+        inputs=input_pdf_path,
+        outputs=output_summary,
+        title="MedReport",
+        description="Enter path to MedReport"
+    )
+
+    iface.launch()
+
+
+if __name__=="__main__":
+    main()
