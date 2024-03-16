2fe8413cc7ce61aa45677dd8655cbdaa1b25ce85
Ashlo <ashutoshbele5@gmail.com>
2024-03-17 02:03:21 +0530

    OpenAi integration

--- rough/pdf_summary.py
+++ rough/pdf_summary.py
@@ -3,8 +3,11 @@ import gradio as gr
 from langchain_community.llms import Ollama
 from langchain.document_loaders import PyPDFLoader
 from langchain.chains.summarize import load_summarize_chain
+from langchain_openai import ChatOpenAI
+
 #model = llama2
-llm = Ollama(model='medllama2')
+#llm = Ollama(model='medllama2')
+llm=ChatOpenAI(temperature=0.7)
 #loader = PyPDFLoader("book.pdf")
 
 #documents = loader.load()
@@ -16,6 +19,10 @@ def summarize_pdf(pdf_file_path, custom_prompt=""):
     summary = chain.invoke(docs)
     summary = format_summary_to_bullet_points(summary)
     return summary
+
+def rag(pdf_file_path, llm):
+    # create a rag where we can ask question and answer related to doc
+    pass
 
 def format_summary_to_bullet_points(summary_data):
     # Extract the 'output_text' field from the summary data
@@ -35,10 +42,11 @@ def format_summary_to_bullet_points(summary_data):
 def main():
     input_pdf_path = gr.File(label="Upload PDF")
     output_summary=  gr.Textbox(label="Summary")
+    user_input = gr.Textbox(label="Chat with your Medical Report")
 
     iface = gr.Interface(
         fn=summarize_pdf,
-        inputs=input_pdf_path,
+        inputs=[input_pdf_path,user_input],
         outputs=output_summary,
         title="MedReport",
         description="Enter path to MedReport"
